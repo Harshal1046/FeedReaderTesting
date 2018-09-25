025369de45ab801db0b488ef333bd3c9b858b297
Harshal Patil <patilharshal288@gmail.com>
2018-09-25 21:43:55 +0530

    test added for feed selection and initial entries

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -85,6 +85,7 @@ $(function() {
 
 
     /* TODO: Write a new test suite named "Initial Entries" */
+    describe("Initial Entries", function() {
 
         /* TODO: Write a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
@@ -92,14 +93,34 @@ $(function() {
          * Remember, loadFeed() is asynchronous so this test will require
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
+        beforeEach(function(done) {
+            loadFeed(0, function() {
+                done();
+            });
+        });
 
-
+        it('To ensure that there is atleast 1 entry', function() {
+            expect($('.entry .feed')).not.toBe(0);
+        });
+    });
 
 
     /* TODO: Write a new test suite named "New Feed Selection" */
+    describe('New Feed Selection', function() {
 
         /* TODO: Write a test that ensures when a new feed is loaded
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
          */
+        beforeEach(function(done) {
+            $('.feed').empty();
+            loadFeed(0, function() {
+                entriesStart = $('.feed').find(allFeeds.url);
+                done();
+            });
+        });
+        it('content changes when new feed is loaded', function() {
+            expect(entriesStart).not.toBe(entriesEnd);
+        });
+    });
 }());
